8078db935d46b6d80afcff78e584958811079fe5
Bryce Sigsbee <btsigsbee@gmail.com>
2020-04-21 14:27:40 -0400

    Updates

---
+++ precache-manifest.3a43048e148fbc034d344ecf5656744c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f675e9de82ef1b5823c5dd68203f9610",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d651c113a3b2a794878b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe32f69f5920f94748dd",
+    "url": "/static/js/2.71c66c4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71c66c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d651c113a3b2a794878b",
+    "url": "/static/js/main.b0c694e8.chunk.js"
+  },
+  {
+    "revision": "2fefc2c946024fb666ad",
+    "url": "/static/js/runtime-main.c28fd3fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
