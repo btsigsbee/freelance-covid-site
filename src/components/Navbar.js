b02ec5295dc5766554c33c2a41b9825244ca54d1
Bryce Sigsbee <btsigsbee@gmail.com>
2020-04-22 10:52:12 -0400

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,10 @@ class NavBar extends Component {
         return(
             <div className='container bg-color' fluid="true">
     <Navbar expand='true' className="font-signika d-flex flex-wrap h2 bg-color font-color-dark justify-content-xs-around">
-      <NavLink exact to={process.env.PUBLIC_URL+"/"}className=' col-xs-12 col-sm-12 col-md-3  text-center text-wrap  font-color-dark nav-item '><h4>Study Overview</h4></NavLink>
-      <NavLink to={process.env.PUBLIC_URL+"/eligibility"}className=' col-xs-12 col-sm-12 col-md-3  text-center font-color-dark nav-item '><h4>Eligibility</h4></NavLink>
-      <NavLink to= {process.env.PUBLIC_URL+"/participation"}className='col-xs-12 col-sm-12 col-md-3 col  text-center  font-color-dark nav-item '><h4>Participation</h4></NavLink>
-      <NavLink to={process.env.PUBLIC_URL+"/researchers"}className='col-xs-12 col-sm-12 col-md-3 text-wrap text-center font-color-dark nav-item '><h4>The Researchers</h4></NavLink>
+      <NavLink exact to={process.env.PUBLIC_URL+"/"}className=' col-xs-12 col-sm-12 col-md-3  text-center text-wrap  font-color-dark nav-item '><h3>Study Overview</h3></NavLink>
+      <NavLink to={process.env.PUBLIC_URL+"/eligibility"}className=' col-xs-12 col-sm-12 col-md-3  text-center font-color-dark nav-item '><h3>Eligibility</h3></NavLink>
+      <NavLink to= {process.env.PUBLIC_URL+"/participation"}className='col-xs-12 col-sm-12 col-md-3 col  text-center  font-color-dark nav-item '><h3>Participation</h3></NavLink>
+      <NavLink to={process.env.PUBLIC_URL+"/researchers"}className='col-xs-12 col-sm-12 col-md-3 text-wrap text-center font-color-dark nav-item '><h3>The Researchers</h3></NavLink>
     </Navbar>
     
     
