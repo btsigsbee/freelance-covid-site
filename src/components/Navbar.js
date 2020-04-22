d2f1bfc135301bf4d58e4fcd4b61412a94c22c2e
Bryce Sigsbee <btsigsbee@gmail.com>
2020-04-22 10:46:07 -0400

    font-size change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class NavBar extends Component {
     render() {
         return(
             <div className='container bg-color' fluid="true">
-    <Navbar expand='true' className="font-signika d-flex flex-wrap h4 bg-color font-color-dark justify-content-xs-around">
+    <Navbar expand='true' className="font-signika d-flex flex-wrap h2 bg-color font-color-dark justify-content-xs-around">
       <NavLink exact to={process.env.PUBLIC_URL+"/"}className=' col-xs-12 col-sm-12 col-md-3  text-center text-wrap  font-color-dark nav-item '>Study Overview</NavLink>
       <NavLink to={process.env.PUBLIC_URL+"/eligibility"}className=' col-xs-12 col-sm-12 col-md-3  text-center font-color-dark nav-item '>Eligibility</NavLink>
       <NavLink to= {process.env.PUBLIC_URL+"/participation"}className='col-xs-12 col-sm-12 col-md-3 col  text-center  font-color-dark nav-item '>Participation</NavLink>
